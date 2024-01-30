69b9dfee8f023440a15f203807de0693b0f3c1aa
Brandt <bseibs>
2024-01-29 17:01:02 -0800

    finished head code

---
+++ head.js
@@ -0,0 +1,11 @@
+const assertEqual = function (expected, actual) {
+  if (expected === actual)
+    console.log(`Assertion Passed: [actual] === [expected]`);
+  else console.log(`Assertion Failed: [actual] !== [expected]`);
+}
+function head(array) {
+  return array[0]
+
+}
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
