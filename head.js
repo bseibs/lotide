18afed5b7cf7eb745425e4f6a6f3d98c0fa44104
Brandt <bseibs>
2024-03-19 16:27:36 -0700

    head.finished

--- head.js
+++ head.js
@@ -1,7 +1,6 @@
-const assertEqual = require('./assertEqual');
 function head(array) {
   return array[0]
 
 }
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
