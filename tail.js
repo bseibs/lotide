92f6420db2f506fcfea3df62da4bec2f9f5c81a9
Brandt <bseibs>
2024-03-19 16:51:15 -0700

    finished

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function (expected, actual) {
-  if (expected === actual) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 const tail = function (arr) {
   let tailArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -13,19 +5,4 @@ const tail = function (arr) {
   }
   return tailArr;
 };
-
-// Test cases
-const arr1 = [1, 2, 3, 4];
-const arr2 = ["hello", "world", "foo", "bar"];
-
-// Test cases for comparing individual elements and array length
-assertEqual(tail(arr1)[0], 2);
-assertEqual(tail(arr1).length, 3);
-assertEqual(tail(arr2)[0], "world");
-assertEqual(tail(arr2).length, 3);
-
-// Test cases for arrays with one or zero elements
-assertEqual(tail([1]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
-
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,15 @@
 const assertEqual = require('../assertEqual')
 const tail = require('../tail')
-let tailArr = []
-const tail = function (arr) {
-  for (const item of arr) {
-    if (item !== arr[0]) {
-      tailArr.push(item);
-    }
-  }
-  return tailArr;
-};
+// Test cases
+const arr1 = [1, 2, 3, 4];
+const arr2 = ["hello", "world", "foo", "bar"];
+
+// Test cases for comparing individual elements and array length
+assertEqual(tail(arr1)[0], 2);
+assertEqual(tail(arr1).length, 3);
+assertEqual(tail(arr2)[0], "world");
+assertEqual(tail(arr2).length, 3);
+
+// Test cases for arrays with one or zero elements
+assertEqual(tail([1]).length, 0); // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0);
