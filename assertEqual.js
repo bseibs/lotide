561921889ec8a2981e9b7af683f0048bde5a0b1e
Brandt <bseibs>
2024-03-19 16:09:50 -0700

    completed.assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,9 @@
-const assertEqual = function (expected, actual) {
-  if (expected === actual)
-      console.log(`Assertion Passed: [actual] === [expected]`);
-    else console.log(`Assertion Failed: [actual] !== [expected]`);
+const assertEqual = function (expected, actual){
 
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+if (expected === actual){
+console.log(`Assertion Passed: [actual] === [expected]`);
+} else {
+  console.log(`Assertion Failed: [actual] !== [expected]`);
+  }
+}
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,8 +1,4 @@
-const assertEqual = function (expected, actual) {
-  if (expected === actual)
-    console.log(`Assertion Passed: [actual] === [expected]`);
-  else console.log(`Assertion Failed: [actual] !== [expected]`);
-}
+const assertEqual = require('./assertEqual');
 function head(array) {
   return array[0]
 
