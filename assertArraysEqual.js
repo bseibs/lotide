213acc0d82cf934019f91e12e2222fd72ffaa7e7
Brandt <bseibs>
2024-03-19 17:50:44 -0700

    done

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,29 +2,17 @@ function eqArrays(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  
+
+  // Check each element of the arrays
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      return false; // If any elements are not equal, return false
     }
   }
-  
-  return true;
+
+  return true; // If all elements are equal, return true
 }
 
-function assertArraysEqual(arr1, arr2) {
-  const isEqual = eqArrays(arr1, arr2);
-  if (isEqual) {
-    console.log(`Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-}
+console.log(`Assertion Passed: [arr1] === [arr2]`);
+console.log(`Assertion Failed: [arr1] !== [arr2]`);
 
-// Example usage:
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Output: Assertion Passed: [1,2,3] === [1,2,3]
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Output: Assertion Failed: [1,2,3] !== [3,2,1]
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Output: Assertion Passed: [1,2,3] === [1,2,3]
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Output: Assertion Failed: [1,2,3] !== [1,2,3]
-
-module.exports = assertArraysEqual;
