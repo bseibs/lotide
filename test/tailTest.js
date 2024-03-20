cd5d02a636c91d52e5ce74f62a27c259b1ed302a
Brandt <bseibs>
2024-03-19 21:13:20 -0700

    tail finished

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,31 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
-// Test cases
-const arr1 = [1, 2, 3, 4];
-const arr2 = ["hello", "world", "foo", "bar"];
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-// Test cases for comparing individual elements and array length
-assertEqual(tail(arr1)[0], 2);
-assertEqual(tail(arr1).length, 3);
-assertEqual(tail(arr2)[0], "world");
-assertEqual(tail(arr2).length, 3);
+describe('tail', function() {
+  const arr1 = [1, 2, 3, 4];
+  const arr2 = ["hello", "world", "foo", "bar"];
 
-// Test cases for arrays with one or zero elements
-assertEqual(tail([1]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0);
+  it('should return 2 when the first element of tail([1, 2, 3, 4]) is checked', function() {
+    assert.strictEqual(tail(arr1)[0], 2);
+  });
+
+  it('should return 3 when the length of tail([1, 2, 3, 4]) is checked', function() {
+    assert.strictEqual(tail(arr1).length, 3);
+  });
+
+  it('should return "world" when the first element of tail(["hello", "world", "foo", "bar"]) is checked', function() {
+    assert.strictEqual(tail(arr2)[0], "world");
+  });
+
+  it('should return 3 when the length of tail(["hello", "world", "foo", "bar"]) is checked', function() {
+    assert.strictEqual(tail(arr2).length, 3);
+  });
+
+  it('should return 0 when the length of tail([1]) is checked', function() {
+    assert.strictEqual(tail([1]).length, 0);
+  });
+
+  it('should return 0 when the length of tail([]) is checked', function() {
+    assert.strictEqual(tail([]).length, 0);
+  });
+});
