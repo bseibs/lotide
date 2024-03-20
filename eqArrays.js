913fbdb4d792d754a6e6f86ddf56a7e875c8b50e
Brandt <bseibs>
2024-03-19 17:18:44 -0700

    updated

--- eqArrays.js
+++ eqArrays.js
@@ -1,35 +1,18 @@
-const assertEqual = function (expected, actual) {
-  if (expected === actual) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 function eqArrays(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    assertEqual(false, true); // Fail the test if lengths are different
-    return false;
+    return false; // Return false if lengths are different
   }
   
   // Check the elements of each array
   for (let i = 0; i < arr1.length; i++) {
-    // If not equal, fail the test
+    // If not equal, return false
     if (arr1[i] !== arr2[i]) {
-      assertEqual(false, true); // Fail the test if elements are different
       return false;
     }
   }
   
-  // If all elements are equal, pass the test
-  assertEqual(true, true);
+  // If all elements are equal, return true
   return true;
 }
-
-// Test cases
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,8 @@
-const AssertEqual = require('../assertEqual')
-const eqArrays = require('../eqArrays')
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
+const assertEqual = require('./assertEqual');
+const eqArrays = require('../eqArrays');
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should PASS
