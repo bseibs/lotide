732bd32faeebd5a7728d06ccbd4f5b4aea41df5e
Brandt <bseibs>
2024-03-19 17:53:07 -0700

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,19 @@
-function eqArrays(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-
-  // Check each element of the arrays
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false; // If any elements are not equal, return false
+      return false;
     }
   }
+  return true;
+};
 
-  return true; // If all elements are equal, return true
-}
-
-console.log(`Assertion Passed: [arr1] === [arr2]`);
-console.log(`Assertion Failed: [arr1] !== [arr2]`);
-
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
