d10f4f61085f2c7fd2e9f9d568e3bc8dcfc56cbf
Brandt <bseibs>
2024-03-19 21:04:10 -0700

    finsihed

--- test/headTest.js
+++ test/headTest.js
@@ -1,18 +1,20 @@
 const assert = require('chai').assert;
 const head   = require('../head');
 
-describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
-it("returns '5' for ['5']", () => {
-  assert.strictEqual(head(['5']), '5'); 
-});
-it('returns 5 when the input array is [5, 6, 7]', function() {
-  assert.strictEqual(head([5, 6, 7]), 5);
-});
 
-it('returns "Hello" when the input array is ["Hello", "Lighthouse", "Labs"]', function() {
-  assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it('returns 5 when the input array is [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('returns "Hello" when the input array is ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
 });
