0c6a5f6c5fff68d44be2e3d2627de1f036c31858
Brandt <bseibs>
2024-03-19 21:22:29 -0700

    finished

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,24 @@
-const { middle, assertArraysEqual } = require('../middle');
+const assert = require('chai').assert;
+const middle = require('../middle');
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(middle([1, 2, 3]), [2]); 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe('middle', function() {
+  it('should return [2] for middle([1, 2, 3])', function() {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return [2, 3] for middle([1, 2, 3, 4])', function() {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('should return [] for middle([1])', function() {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('should return [] for middle([])', function() {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it('should return ["world", "foo"] for middle(["hello", "world", "foo", "bar"])', function() {
+    assert.deepEqual(middle(["hello", "world", "foo", "bar"]), ["world", "foo"]);
+  });
+});
